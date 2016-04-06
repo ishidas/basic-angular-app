08f19e8c24e773f832a620180ca16a248bdc7ac1
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-05 17:02:21 -0700

    delete text button added

--- controller.js
+++ controller.js
@@ -1,4 +1,7 @@
 module.exports = angular.module('myApp', [])
 .controller('mainCtrl', function (){
   this.input = 'Please input something here.'
+  // this.deleteStuff = function(input){
+  //   return null
+  // }
 });
